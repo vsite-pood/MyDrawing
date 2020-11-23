43eb28d113876e19ab90f546b02aebe6611c944b
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-11-23 18:31:12 +0100

    Drawing

---
+++ Model/Drawing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Pood.MyDrawing.Model
+{
+    public class Drawing : IDrawable
+    {
+        private List<Shape> shapes = new List<Shape>();
+
+        public void Draw(IGraphics graphics)
+        {
+            foreach (var shape in shapes) 
+            {
+                shape.Draw(graphics);
+            }
+        }
+        public void Add(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+        public void Remove(Shape shape)
+        {
+            shapes.Remove(shape);
+        }
+        public void Clear()
+        {
+            shapes.Clear();
+        }
+    }
+}
