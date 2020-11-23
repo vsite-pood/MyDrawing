e8aa3412f73f23986d2e8ab1c558667943eda4c7
Mislav <mislav.franja@gmail.com>
2020-11-23 18:31:12 +0100

    Add Drawing class

---
+++ Model/Drawing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.POOD.MyDrowing.Model
+{
+    public class Drawing : IDrawable
+    {
+        private List<Shape> shapes = new List<Shape>();
+
+        public void Draw(IGraphics graphics)
+        {
+            foreach  (Shape element in shapes)
+            {
+                element.Draw(graphics);
+            }
+        }
+
+        public void Add(Shape s) => shapes.Add(s);
+        public void Remove(Shape s) => shapes.Remove(s);
+        public void Clear() => shapes.Clear();
+    }
+}
