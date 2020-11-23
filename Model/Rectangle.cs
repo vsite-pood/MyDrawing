7cafab18b01e9943df07e11c07ac8928014db592
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-11-23 18:24:26 +0100

    create rect class

--- Model/Rectangle.cs
+++ Model/Rectangle.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace Vsite.Pood.MyDrawing.Model
 {
-    public interface IGraphics
+    public class Rectangle : Shape, IDrawable
     {
-        void DrawRectangle(Rectangle rectangle);
+        public override void Draw(IGraphics graphics)
+        {
+            graphics.DrawRectangle(this);
+        }
     }
 }
