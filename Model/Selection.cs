a4ccbe581d1568b3f2f2ef8e0478eceb84e3b85f
Mislav Franjo <mislav.franja@gmail.com>
2020-11-30 18:45:14 +0100

    added Dictionary in selection with all hadles

--- Model/Selection.cs
+++ Model/Selection.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace Vsite.POOD.MyDrowing.Model
 {
-    public class Selection : IMovable, IResizeble
+    public class Selection : IMovable, IResizeble, IDrawable
     {
         private readonly List<Shape> shapes = new List<Shape>();
+        private readonly Dictionary<ResizeDirection, ResizeHandle> handles = new Dictionary<ResizeDirection, ResizeHandle>()
+        {
+            { ResizeDirection.NorthWest, new ResizeHandle() },
+            { ResizeDirection.NorthEast, new ResizeHandle() },
+            { ResizeDirection.North, new ResizeHandle() },
+            { ResizeDirection.East, new ResizeHandle() },
+            { ResizeDirection.West, new ResizeHandle() },
+            { ResizeDirection.South, new ResizeHandle() },
+            { ResizeDirection.SouthEast, new ResizeHandle() },
+            { ResizeDirection.SouthWest, new ResizeHandle() },
+        };
         public void Add(Shape shape) 
         {
             if (!shapes.Contains(shape))
@@ -28,6 +39,14 @@ namespace Vsite.POOD.MyDrowing.Model
         {
             foreach (Shape s in shapes)
                 s.Resize(direction, dx, dy);
+        }
+
+        public void Draw(IGraphics graphics)
+        {
+            // draw all shapes in selection
+            // arrange handeles 
+            // draw handels
+            throw new NotImplementedException();
         }
     }
 }
