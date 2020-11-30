8e66320296d695a225c281cd4f7e5c6db5c80b22
Julijan Sribar <julijan.sribar@gmail.com>
2020-11-30 18:45:27 +0100

    Implement Selection handles

--- Model/Selection.cs
+++ Model/Selection.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace Vsite.Pood.MyDrawing.Model
 {
-    public class Selection : IMovable, IResizable
+    public class Selection : IMovable, IResizable, IDrawable
     {
         private readonly List<Shape> shapes = new List<Shape>();
+        private readonly Dictionary<ResizeDirection, ResizeHandle> handles = new Dictionary<ResizeDirection, ResizeHandle>()
+        { 
+            { ResizeDirection.North, new ResizeHandle() },
+            { ResizeDirection.NorthEast, new ResizeHandle() },
+            { ResizeDirection.East, new ResizeHandle() },
+            { ResizeDirection.SouthEast, new ResizeHandle() },
+            { ResizeDirection.South, new ResizeHandle() },
+            { ResizeDirection.SouthWest, new ResizeHandle() },
+            { ResizeDirection.West, new ResizeHandle() },
+            { ResizeDirection.NorthWest, new ResizeHandle() }
+        };
 
         public void Add(Shape shape)
         {
@@ -37,6 +48,13 @@ namespace Vsite.Pood.MyDrawing.Model
         {
             foreach (Shape shape in shapes)
                 shape.Resize(direction, dx, dy);
+        }
+
+        public void Draw(IGraphics graphics)
+        {
+            // draw all shapes in selection
+            // arrange handles
+            // draw handles
         }
     }
 }
