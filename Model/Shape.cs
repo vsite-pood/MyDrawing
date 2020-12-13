48d8c8f609eec77c5f9f4cc9ec976d557deb0e65
Julijan Sribar <julijan.sribar@gmail.com>
2020-12-13 23:59:58 +0100

    Add IHittable interface

--- Model/Shape.cs
+++ Model/Shape.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 
 namespace Vsite.Pood.MyDrawing.Model
 {
-    public abstract class Shape : IDrawable, IMovable, IResizable, IBounding
+    public abstract class Shape : IDrawable, IMovable, IResizable, IBounding, IHittable
     {
         public abstract RectangleF BoundingRectangle { get; }
+        public abstract bool Contains(float x, float y);
         public abstract void Draw(IGraphics graphics);
         public abstract void Move(float dx, float dy);
         public abstract void Resize(ResizeDirection direction, float dx, float dy);
