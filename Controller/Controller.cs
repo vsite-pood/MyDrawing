10990fddec8d750a5b319801264abafb7e5c6abe
Mislav <mislav.franja@gmail.com>
2020-12-14 18:09:55 +0100

    add controller

---
+++ Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.POOD.MyDrowing.Model;
+
+namespace Vsite.POOD.MyDrowing.Controller
+{
+    public class Controller
+    {
+        Drawing drawing;
+        public Controller(Drawing myDrowing) 
+        {
+            this.drawing = myDrowing;
+        }
+        public void AddShape(Shape shape) 
+        {
+            drawing.Add(shape);
+        }
+
+    }
+}
