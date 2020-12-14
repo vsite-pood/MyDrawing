7e1db8ee5c19df53090f66c6d7b62cf959f19ae8
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-12-14 18:10:02 +0100

    Add Controller

---
+++ Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.pood.MyDrawing.Model;
+
+namespace Vsite.Pood.MyDrawing.Controller
+{
+    public class Controller
+    {
+        Drawing drawing;
+        public Controller(Drawing drawing)
+        {
+            this.drawing = drawing;
+        }
+
+        public void AddShape(Shape shape)
+        {
+            drawing.Add(shape);
+        }
+    }
+}
