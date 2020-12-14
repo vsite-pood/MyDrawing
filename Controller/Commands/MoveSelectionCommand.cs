ea843f7d8cd8acbde9247567c4a6ce9eb1e242bb
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-12-14 18:28:17 +0100

    Add ICommand interface and few concrete commands

---
+++ Controller/Commands/MoveSelectionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.pood.MyDrawing.Model;
+
+namespace Vsite.Pood.MyDrawing.Controller.Commands
+{
+    public class MoveSelectionCommand : ICommand
+    {
+        Selection selection;
+        float dx;
+        float dy;
+        public MoveSelectionCommand(Selection selection, float dx, float dy)
+        {
+            this.selection = selection;
+            this.dx = dx;
+            this.dy = dy;
+        }
+        public void Execute()
+        {
+            selection.Move(dx, dy);
+        }
+
+        public void Undo()
+        {
+            selection.Move(-dx, -dy);
+        }
+    }
+}
