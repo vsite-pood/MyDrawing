9f86c588021539791a5f543513faf0cf0549d527
Mislav <mislav.franja@gmail.com>
2020-12-14 19:07:45 +0100

    add undo redo stacks

---
+++ Controller/Commands/UndoRedoStacks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.POOD.MyDrowing.Model;
+
+namespace Vsite.POOD.MyDrowing.Controller
+{
+
+    public class UndoRedoStacks
+    {
+        public void Add(ICommand command) 
+        {
+            undoStack.Push(command);
+            redoStack.Clear();
+        }
+        public void Undo() 
+        {
+            var command = undoStack.Pop();
+            command.Undo();
+            redoStack.Push(command);
+        }
+        public void Redo() 
+        {
+            var command = redoStack.Pop();
+            command.Execute();
+            redoStack.Push(command);
+        }
+        public bool CanUndo() {
+            return undoStack.Count > 0;
+        }
+
+        public bool CanRedo()
+        {
+            return redoStack.Count > 0;
+        }
+        private Stack<ICommand> undoStack = new Stack<ICommand>();
+        private Stack<ICommand> redoStack = new Stack<ICommand>();
+    }
+}
---
+++ UnitTests/TestUndoredoStacks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.POOD.MyDrowing.Controller;
+using Vsite.POOD.MyDrowing.Model;
+
+namespace Vsite.POOD.MyDrowing.UnitTests
+{
+    [TestClass]
+    public class TestUndoredoStacks
+    {
+        [TestMethod]
+        public void UndoAndRedoMoveBetwweUndoAndredoStack()
+        {
+            UndoRedoStacks stack = new UndoRedoStacks();
+            Drawing drawing = new Drawing();
+            Shape shape = new Rectangle(10, 20, 30, 40);
+            ICommand command = new AddShapeCommand(drawing, shape);
+            stack.Add(command);
+            Assert.IsTrue(stack.CanUndo());
+            Assert.IsFalse(stack.CanRedo());
+            stack.Undo();
+            Assert.IsTrue(stack.CanRedo());
+            Assert.IsFalse(stack.CanUndo());
+            stack.Redo();
+            Assert.IsTrue(stack.CanUndo());
+            Assert.IsFalse(stack.CanRedo());
+        }
+
+        [TestMethod]
+        public void UndoRevertsLastCommandAdded()
+        {
+            UndoRedoStacks stack = new UndoRedoStacks();
+            Drawing drawing = new Drawing();
+            Shape shape = new Rectangle(10, 20, 30, 40);
+            ICommand command = new AddShapeCommand(drawing, shape);
+            stack.Add(command);
+            Assert.IsTrue(stack.CanUndo());
+            Assert.IsFalse(stack.CanRedo());
+            stack.Undo();
+            Assert.IsTrue(stack.CanRedo());
+            Assert.IsFalse(stack.CanUndo());
+            stack.Redo();
+            Assert.IsTrue(stack.CanUndo());
+            Assert.IsFalse(stack.CanRedo());
+        }
+    }
+}
