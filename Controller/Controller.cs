4c290a4513d466eabfb1b6023d8bad320819c7c3
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-12-14 18:10:06 +0100

    added kontroller

---
+++ Controller/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.Pood.MyDrawing.Model;
+
+namespace Vsite.Pood.MyDrawing.Controller
+{
+    public class Controller
+    {
+        Drawing drawing;
+        public Controller(Drawing drawing) 
+        {
+            this.drawing = drawing;
+        }
+        public void AddShape(Shape shape)
+        {
+            drawing.Add(shape);
+        }
+    }
+}
