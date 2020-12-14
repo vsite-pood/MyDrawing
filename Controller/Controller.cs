432ce69aba9a4755494935785edf67c4413bd1a3
Ante Mišković <ante.miskovic@adnet.hr>
2020-12-14 18:10:05 +0100

    Add controller

---
+++ Controller/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Vsite.Pood.MyDrawing.Model;
+
+namespace Vsite.Pood.MyDrawing.Controller
+{
+	public class Controller
+	{
+		Drawing drawing;
+		public Controller(Drawing drawing )
+		{
+			this.drawing = drawing;
+		}
+
+		public void AddShape(Shape shape)
+		{
+			drawing.Add( shape );
+		}
+
+	}
+}
