2f4a2293c2b4ef63669298203f4a82a00591ae63
Mislav <mislav.franja@gmail.com>
2020-12-14 18:28:16 +0100

    add icommands interface and few commands

---
+++ Controller/Commands/AddShapeCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.POOD.MyDrowing.Model;
+
+namespace Vsite.POOD.MyDrowing.Controller
+{
+    public class AddShapeCommand : ICommand
+    {
+        private Drawing drawing;
+        private Shape shape;
+        public AddShapeCommand(Drawing drowing,Shape shape) 
+        {
+            this.drawing = drowing;
+            this.shape = shape;
+        }
+        public void Execute()
+        {
+            drawing.Add(shape);
+        }
+
+        public void Undo()
+        {
+            drawing.Remove(shape);
+        }
+    }
+}
---
+++ Controller/Commands/MoveSelectionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.POOD.MyDrowing.Model;
+
+namespace Vsite.POOD.MyDrowing.Controller
+{
+    public class MoveSelectionCommand : ICommand
+    {
+        private Selection selection;
+        private float dx, dy;
+        public MoveSelectionCommand(Selection selection,float dx,float dy)
+        {
+            this.selection = selection;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        public void Execute()
+        {
+            selection.Move(dx, dy);
+        }
+
+        public void Undo()
+        {
+            selection.Move(-dx, -dy);
+        }
+    }
+}
