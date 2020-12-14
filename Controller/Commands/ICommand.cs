df951d74d415c84f32658fccda613d17e8e16ac4
Julijan Sribar <julijan.sribar@gmail.com>
2020-12-14 18:28:18 +0100

    Add ICommand interface and few concrete commands

---
+++ Controller/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viste.Pood.MyDrawing.Controller
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
---
+++ Controller/Commands/MoveSelectionCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.Pood.MyDrawing.Model;
+
+namespace Viste.Pood.MyDrawing.Controller.Commands
+{
+    public class MoveSelectionCommand : ICommand
+    {
+        Selection selection;
+        float dx;
+        float dy;
+
+        public MoveSelectionCommand(Selection selection, float dx, float dy)
+        {
+            this.selection = selection;
+            this.dx = dx;
+            this.dy = dy;
+        }
+        public void Execute()
+        {
+            selection.Move(dx, dy);
+        }
+
+        public void Undo()
+        {
+            selection.Move(-dx, -dy);
+        }
+    }
+}
