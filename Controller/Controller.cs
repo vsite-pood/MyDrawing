4c72e92e963b7d3acb51d1b414a40380740485eb
Julijan Sribar <julijan.sribar@gmail.com>
2020-12-14 18:10:01 +0100

    Add Controller

---
+++ Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.Pood.MyDrawing.Model;
+
+namespace Viste.Pood.MyDrawing.Controller
+{
+    public class Controller
+    {
+        Drawing drawing;
+        public Controller(Drawing drawing)
+        {
+            this.drawing = drawing;
+        }
+
+        public void AddShape(Shape shape)
+        {
+            drawing.Add(shape);
+        }
+    }
+}
