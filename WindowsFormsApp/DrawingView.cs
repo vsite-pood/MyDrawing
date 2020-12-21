d8604bbca5f8ec61c829ce885c4c175ae46f4ce4
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-12-21 19:02:27 +0100

    add view and controller in WinForms application

---
+++ WindowsFormsApp/DrawingView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vsite.Pood.MyDrawing.WinForms
+{
+    class DrawingView : PictureBox
+    {
+        public DrawingView(WinFormsController controller)
+        {
+            this.controller = controller;
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            controller.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            controller.OnMouseMove(e);
+
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            controller.OnMouseUp(e);
+
+        }
+        private WinFormsController controller;
+    }
+}
---
+++ WindowsFormsApp/WinFormsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vsite.pood.MyDrawing.Model;
+using Vsite.Pood.MyDrawing.Controller;
+
+namespace Vsite.Pood.MyDrawing.WinForms
+{
+    class WinFormsController : Controller.Controller
+    {
+        public enum MouseClickMode
+        {
+            Selection,
+            InsertRectangle,
+            InsertEllipse
+        }
+        public MouseClickMode ClickMode { get; set; }
+        public WinFormsController(Drawing drawing) : base(drawing)
+        {
+ 
+        }
+        public void OnMouseDown(MouseEventArgs e)
+        {
+            switch (ClickMode)
+            {
+                case MouseClickMode.Selection:
+                    Shape shape = drawing.GetHitShape(e.X, e.Y);
+                    if(shape != null)
+                    {
+                        drawing.Selection.Add(shape);
+                    }
+                    break;
+            }
+        }
+
+        public void OnMouseMove(MouseEventArgs e)
+        {
+
+        }
+
+        public void OnMouseUp(MouseEventArgs e)
+        {
+
+        }
+    }
+}
