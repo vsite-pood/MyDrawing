ef972d6d2fc9d8eef3c56595e43feb3335a562d8
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-12-21 18:08:41 +0100

    implement and test undo redo stack

--- UnitTests/TestUndoRedoStacks.cs
+++ UnitTests/TestUndoRedoStacks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Vsite.pood.MyDrawing.Model;
 using Vsite.Pood.MyDrawing.Controller;
@@ -33,14 +34,45 @@ namespace vsite.pood.MyDrawing.UnitTests
             Drawing drawing = new Drawing();
             Shape shape = new Rectangle(10, 20, 30, 40);
             ICommand command = new AddShapeCommand(drawing, shape);
+            command.Execute();
             stack.Add(command);
-            Assert.IsTrue(stack.CanUndo());
-            Assert.IsFalse(stack.CanRedo());
+            Assert.IsTrue(drawing.Shapes.Contains(shape));
+            stack.Undo();
+            Assert.IsFalse(drawing.Shapes.Contains(shape));
+            stack.Redo();
+            Assert.IsTrue(drawing.Shapes.Contains(shape));
+        }
+        [TestMethod]
+        public void RedoReappliesCommandUndone()
+        {
+            UndoRedoStacks stack = new UndoRedoStacks();
+            Drawing drawing = new Drawing();
+            Shape shape = new Rectangle(10, 20, 30, 40);
+            ICommand command = new AddShapeCommand(drawing, shape);
+            command.Execute();
+            stack.Add(command);
+            Assert.IsTrue(drawing.Shapes.Contains(shape));
+            stack.Undo();
+            Assert.IsFalse(drawing.Shapes.Contains(shape));
+            stack.Redo();
+            Assert.IsTrue(drawing.Shapes.Contains(shape));
+        }
+
+        [TestMethod]
+        public void NewCommandClearsRedoStack()
+        {
+            UndoRedoStacks stack = new UndoRedoStacks();
+            Drawing drawing = new Drawing();
+            Shape shape = new Rectangle(10, 20, 30, 40);
+            ICommand command = new AddShapeCommand(drawing, shape);
+            command.Execute();
+            stack.Add(command);
             stack.Undo();
             Assert.IsTrue(stack.CanRedo());
-            Assert.IsFalse(stack.CanUndo());
-            stack.Redo();
-            Assert.IsTrue(stack.CanUndo());
+            Shape shape2 = new Ellipse(20, 40, 60, 80);
+            ICommand command2 = new AddShapeCommand(drawing, shape2);
+            command2.Execute();
+            stack.Add(command2);
             Assert.IsFalse(stack.CanRedo());
         }
     }
