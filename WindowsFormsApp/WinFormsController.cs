0ea6c041fb6b1c4cd6e2a4e0bca6d7a20e6c7759
Ante Mišković <ante.miskovic@adnet.hr>
2020-12-21 19:02:29 +0100

    Add View and Controller in winforms application

---
+++ WindowsFormsApp/WinFormsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vsite.Pood.MyDrawing.WinForms
+{
+	class WinFormsController : Controller.Controller
+	{
+		public enum MouseClickMode
+		{
+			Selection,
+			InsertRectangle,
+			InsertEllipse
+		}
+		public MouseClickMode ClickMode { get; set; }
+		public WinFormsController(Model.Drawing drawing) : base( drawing )
+		{
+
+		}
+		public void OnMouseDown(MouseEventArgs e)
+		{
+			switch ( ClickMode )
+			{
+				case MouseClickMode.Selection:
+					Model.Shape shape = drawing.GetHitShape(e.X,e.Y);
+					if ( shape != null )
+						drawing.Selection.Add( shape );
+					break;
+				case MouseClickMode.InsertRectangle:
+				case MouseClickMode.InsertEllipse:
+					break;
+			}
+		}
+		public void OnMouseMove(MouseEventArgs e)
+		{
+
+		}
+		public void OnMouseUp(MouseEventArgs e)
+		{
+
+		}
+	}
+}
