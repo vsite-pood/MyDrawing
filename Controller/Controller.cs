2db3f7a6dd306ed0d64a2ef9459830f489f156a7
Ante Mišković <ante.miskovic@adnet.hr>
2020-12-21 18:08:36 +0100

    Implement and test Undo/Redo stack.

--- Controller/Controller.cs
+++ Controller/Controller.cs
@@ -6,6 +6,8 @@ namespace Vsite.Pood.MyDrawing.Controller
 	public class Controller
 	{
 		Drawing drawing;
+		UndoRedoStacks undoRedo = new UndoRedoStacks();
+
 		public Controller(Drawing drawing)
 		{
 			this.drawing = drawing;
@@ -14,13 +16,25 @@ namespace Vsite.Pood.MyDrawing.Controller
 		public void AddShape(Shape shape)
 		{
 			ICommand command = new AddShapeCommand( drawing, shape );
+			command.Execute();
+			undoRedo.Add( command );
 		}
 
 		public void MoveSelection(float dx, float dy)
 		{
 			ICommand command = new MoveSelectionCommand( drawing.Selection, dx, dy );
 			command.Execute();
-
+			undoRedo.Add( command );
 		}
+		public void Undo()
+		{
+			undoRedo.Undo();
+		}
+		public void Redo()
+		{
+			undoRedo.Redo();
+		}
+		public bool CanUndo => undoRedo.CanUndo();
+		public bool CanRedo => undoRedo.CanRedo();
 	}
 }
